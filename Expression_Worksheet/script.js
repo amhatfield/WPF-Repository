60bb14b91c202b490325efdbc80e3197db685b54
Amanda Hatfield <amhatfield@fullsail.edu>
2013-09-07 18:15:21 -0500

    started

---
+++ Expression_Worksheet/script.js
@@ -0,0 +1,8 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: amandahatfield
+ * Date: 9/7/13
+ * Time: 6:09 PM
+ * To change this template use File | Settings | File Templates.
+ */
+//Amanda Hatfield, 9/7/2013 and Expression worksheet
