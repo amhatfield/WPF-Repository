108dbb635ac1d553c3ef90f580923a4af592fe49
Amanda Hatfield <amhatfield@fullsail.edu>
2013-09-07 19:19:27 -0500

    trying it out

--- Expression_Worksheet/script.js
+++ Expression_Worksheet/script.js
@@ -5,4 +5,9 @@
  * Time: 6:09 PM
  * To change this template use File | Settings | File Templates.
  */
-//Amanda Hatfield, 9/7/2013 and Expression worksheet
+//Amanda Hatfield, 9/7/2013 and Expression worksheet.
+
+//Dog Years
+
+var SparksAge;
+SparksAge = 14 * 7;
