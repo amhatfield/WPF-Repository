20637fad24e7ff77b6aa5eb1a1680b4e9aa271d1
Amanda Hatfield <amhatfield@fullsail.edu>
2013-09-08 09:16:50 -0500

    no message

---
+++ Expression_Worksheet/js/script.js
@@ -0,0 +1,15 @@
+//Amanda Hatfield, 9/7/2013 and Expression worksheet.
+
+//Dog Years
+
+// figure out what the dog age is
+var human = 4;
+var  dog = 7;
+// time the dog's age in human years by dog years
+var sparky = human * dog;
+// print Sparky age
+console.log(sparky)
+
+// Slice of Pie part 1
+
+
