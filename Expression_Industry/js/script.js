d1768f03bfcc163c2822ecfeca23afe91f134643
Amanda Hatfield <amhatfield@fullsail.edu>
2013-09-10 21:27:01 -0500

    no message

--- Expression_Industry/js/script.js
+++ Expression_Industry/js/script.js
@@ -1,1 +1,5 @@
-alert("This is in the js file");
+// Amanda Hatfield, 9/9/2013 and Expression Industry
+
+// Being the retail industry. I am going to figure out how much a game system would cost if I put it on layaway.
+
+var price = prompt("What is the price of the game system");
