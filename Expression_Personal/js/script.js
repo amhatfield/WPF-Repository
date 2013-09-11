6942d5cbf0c7558db7cc1e629b3ec9e516013510
Amanda Hatfield <amhatfield@fullsail.edu>
2013-09-10 20:55:01 -0500

    no message

--- Expression_Personal/js/script.js
+++ Expression_Personal/js/script.js
@@ -3,8 +3,16 @@
 
  // I am going to calculate how much I need to spend on classroom snacks for my daughter's class.
 
-var  userInput = prompt("number of students in the class");
-var dollarsPreKid = prompt("how much do you want to spend pre kid");
+var  userInput = prompt("Number of students in the class");
+var dollarsPreKid = prompt("How much do you want to spend pre kid");
+var weeks= prompt("Number of weeks you provide snacks");
+// now we will multiply the number of weeks by the dollar pre kid.
+var total = weeks * dollarsPreKid;
+// now we divide the total to how many kids are in the class.
+var result = total / userInput
+// now we print to the console
+console.log("How much I have to spend is" + result "dollars in classroom snacks.");
+
 
 
 
