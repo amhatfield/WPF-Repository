e0ed3d54f7c0fd03dde7a244589e18ec921e15ca
Amanda Hatfield <amhatfield@fullsail.edu>
2013-09-11 09:04:17 -0500

    no message

--- Expression_Industry/js/script.js
+++ Expression_Industry/js/script.js
@@ -3,10 +3,11 @@
 // Being the retail industry. I am going to figure out how much a game system would cost if I put it on layaway.
 
 var price = prompt("What is the price of the game system");
-var downPayment = prompt("What is the percent for the down payment")
-var numberOfWeeks = prompt("What will be the number of payments you will make.");
-// We will multiply the price of the item to the percent
+var downPayment = prompt("What is the percent for the down payment." + /n  "For example, 2% would be enter as 20.");
 var overallTotal = price * downPayment;
+var input = alert("The final price of the game system is"  + overallTotal );
+var amountOfPayment = prompt("How much will to be for the payment");
+// We will multiply the price of the item to the percent
 // Then we will take the overall total divide that to the number of weeks
-var  howManyPayments = numberofWeeks / overallTotal;
-console.log("This is how many payment I will have to make" + howManyPayments);
+var  howManyWeeks = amountOfPayment / overallTotal;
+console.log("This is how many weeks I have to make a payment" + howManyWeeks);
