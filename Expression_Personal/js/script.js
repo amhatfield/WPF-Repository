57148b14bd383dd9e1b5c41e31f9a5fe10203345
Amanda Hatfield <amhatfield@fullsail.edu>
2013-09-10 20:37:07 -0500

    no message

--- Expression_Personal/js/script.js
+++ Expression_Personal/js/script.js
@@ -1,4 +1,9 @@
-// Amanda Hatfield, 9/8/2013 and Expressions (personal)
+// Amanda Hatfield, 9/9/2013 and Expression Personal
+
+
+ // I am going to calculate how much I need to spend on classroom snacks for my daughter's class.
+
+var  userInput = prompt("number of students in the class");
 
 
 
