4a4bfc71031083902a399bf644e77cb10d9ef75e
Amanda Hatfield <amhatfield@fullsail.edu>
2013-09-11 21:20:09 -0500

    no message

--- Expression_Wacky/script.js
+++ Expression_Wacky/script.js
@@ -1,3 +1,7 @@
 // Amanda Hatfield, 9/11/2013 and Expression Wacky
 
+// I am going to calculate how many hours it would take to watch eight seasons of Supernatural.
+var seasons = prompt("Enter the current amount of seasons of Supernatural.");
+var length = prompt ("Enter the length of the show");
+var Total = prompt("Enter the current total of  ")
 
