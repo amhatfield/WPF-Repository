fd4a816d9f52efbdce4ae28722fa58981dd15160
Amanda Hatfield <amhatfield@fullsail.edu>
2013-09-16 16:58:39 -0500

    no message

--- Conditionals_Worksheet/js/script.js
+++ Conditionals_Worksheet/js/script.js
@@ -2,9 +2,9 @@
 
 // Group #1: Last Chance of Gas!
 
-var  gasMileage = 10
-var  gauge = 25
-var  gasCapacity = 100
+var  gasMileage = 10 "mpg"
+var  gauge = 25  "percent"
+var  gasCapacity = 100 "gallons"
 
 // Don't need to get gas.
 if(gasMileage > gauge / gasCapacity){
@@ -20,8 +20,14 @@ if(gasMileage / gasCapacity < gauge){
 
 // Group #2 Check the Login
 
-var userName = prompt("Enter your Username");
-var passWord = prompt("Enter your Password");
+var userName = "angelic_scars96";
+var passWord = "aislynn6960";
+var correctUser = "angelic_scars96";
+var correctPass = "aislynn6960";
+
+// login info is correct!
+
+if()
 
 
 
