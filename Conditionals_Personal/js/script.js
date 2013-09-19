40227c6b70c8d8246538ea185bb86fff566b7a28
Amanda Hatfield <amhatfield@fullsail.edu>
2013-09-18 20:36:09 -0500

    no message

---
+++ Conditionals_Personal/js/script.js
@@ -0,0 +1,15 @@
+// Amanda Hatfield, 9/17/2013 and Conditional Personal
+
+
+// Planning a birthday party for my daughter at Build a Bear
+
+var numberOfGuests = 30
+var minGuests = 20
+
+
+// Have to pay extra per guest
+
+if(numberOfGuests > minGuests ){
+    console.log("You have to pay $30.00 extra per guest")
+
+}
