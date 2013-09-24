51496320d2d4723f00f6f33a1c1a49d460c99711
Amanda Hatfield <amhatfield@fullsail.edu>
2013-09-23 22:05:39 -0500

    no message

--- Functions_Worksheet/js/script.js
+++ Functions_Worksheet/js/script.js
@@ -1,7 +1,10 @@
 // Amanda Hatfield 9/23/2013 Functions_Worksheet
 
 // Circumference
-
-function calcCirc(){
-
+  calcCirc(2)
+function calcCirc(radius){
+   var diameter = 6   // diameter is 6
+   var pi = 3.14     // pi is 3.14
+   var total = diameter * pi   // you will times diameter by pi
+    console.log(total)
 }
