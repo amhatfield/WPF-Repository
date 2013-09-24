ead3c3748735dd2071d03aee1d23c54eb5969227
Amanda Hatfield <amhatfield@fullsail.edu>
2013-09-23 22:17:17 -0500

    no message

--- Functions_Worksheet/js/script.js
+++ Functions_Worksheet/js/script.js
@@ -1,10 +1,13 @@
 // Amanda Hatfield 9/23/2013 Functions_Worksheet
 
 // Circumference
-  calcCirc(2)
+  var circle = calcCirc(2)
 function calcCirc(radius){
    var diameter = 6   // diameter is 6
    var pi = 3.14     // pi is 3.14
    var total = diameter * pi   // you will times diameter by pi
-    console.log(total)
+   return radius * total      // you will times radius by total to get the circumference
+
+
 }
+console.log("The Circumference of the circle is" +  circle)
