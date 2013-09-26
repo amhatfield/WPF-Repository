8707ef630107a31eb294c462ec1d2997e57c8c35
Amanda Hatfield <amhatfield@fullsail.edu>
2013-09-26 09:16:24 -0500

    no message

---
+++ Functions_Personal/js/script.js
@@ -0,0 +1,6 @@
+// Amanda Hatfield 9/25/2013 Functions Personal
+
+//  How much can I spend on each family member for Christmas?
+
+var numberOfFamily = prompt("Enter how many family members you have to buy for.") // I am prompting the user to enter data.
+var budget = prompt("Enter your budget for the christmas.")
