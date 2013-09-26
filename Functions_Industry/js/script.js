0141f2a379a676556ed0d9356bc94a56c9568b47
Amanda Hatfield <amhatfield@fullsail.edu>
2013-09-26 18:25:10 -0500

    no message

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -3,10 +3,11 @@
 
 // We are going to see if we can use a coupon on a sale item.
 
-var salePrice =  5
+var  salePrice = 10;
+
 
 // if the sale price it less then $3.00 then you cant use the 15% off coupon.
 // if the sale price is more then $3.00 then you cant use the 15% off coupon.
 
-(salePrice > 3) ? console.log("You can use the 15% off coupon!") : console.log("You can't use the 15% off coupon.")
+(salePrice > 3) ?  console.log("You can use the 15% off coupon!") : console.log("You can't use the 15% off coupon.");
 
