e0c60f7e5e8e29dc5249b40d9356c3a131e51353
Amanda Hatfield <amhatfield@fullsail.edu>
2013-09-26 09:21:05 -0500

    no message

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -3,4 +3,6 @@
 //  How much can I spend on each family member for Christmas?
 
 var numberOfFamily = prompt("Enter how many family members you have to buy for.") // I am prompting the user to enter data.
-var budget = prompt("Enter your budget for the christmas.")
+var budget = prompt("Enter your budget for the christmas.") // I am prompting the user to enter their budget.
+var total = budget / numberOfFamily // To get the price for each person you will divide
+var overallTotal =  total + numberOfFamily // To get how much you will spend overall you will add.
