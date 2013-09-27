cc643d67c4a526535f0a8c7b6281a214e2db4518
Amanda Hatfield <amhatfield@fullsail.edu>
2013-09-26 19:01:00 -0500

    no message

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -2,9 +2,9 @@
 
 
 // How much do I need to save up to go to Supernatural Con.
-
-function superCon() {
+ superCon(900, 250, 650)
+function superCon(hotelRoom, conPass, activities) {
     var hotelRoom
     var conPass
-
+    var activities
 }
